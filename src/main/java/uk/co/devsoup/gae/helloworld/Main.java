5e3391116037e497f9ad4d2869685a4fe155e134
AndrewG <nobody@devsoup.co.uk>
2014-02-23 16:00:32 +0000

    Initial commit

---
+++ src/main/java/uk/co/devsoup/gae/helloworld/Main.java
@@ -0,0 +1,40 @@
+package uk.co.devsoup.gae.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Java class which outputs HTML to demo that the code works.
+ */
+public class Main extends HttpServlet implements Servlet {
+    /**
+     * Constructor.
+     * <p/>
+     * Does nothing at present.
+     */
+    public Main() {
+    }
+
+    /**
+     * Handle GET requests.
+     *
+     * @param request  The incoming request object.
+     * @param response The outgoing response to modify to incude our custom data.
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Hello from DevSoup</title></head>");
+        out.println("<body>DevSoup's Java Servlet says hello</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
